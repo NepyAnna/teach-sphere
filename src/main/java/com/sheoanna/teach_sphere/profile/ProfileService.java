be6b9ac15573893504899ac8e1b996f751730433
NepyAnna <nepyann@gmail.com>
2025-08-18 14:54:50 +0200

    feat: added ProfileNotFoundException class and findById method to ProfileService

--- src/main/java/com/sheoanna/teach_sphere/profile/ProfileService.java
+++ src/main/java/com/sheoanna/teach_sphere/profile/ProfileService.java
@@ -3,10 +3,12 @@ package com.sheoanna.teach_sphere.profile;
 import com.sheoanna.teach_sphere.cloudinary.CloudinaryService;
 import com.sheoanna.teach_sphere.profile.dtos.ProfileMapper;
 import com.sheoanna.teach_sphere.profile.dtos.ProfileResponse;
+import com.sheoanna.teach_sphere.profile.exceptions.ProfileNotFoundException;
 import com.sheoanna.teach_sphere.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -22,5 +24,13 @@ public class ProfileService {
                 .map(profileMapper::toResponse);
     }
 
+    public ProfileResponse findById(Long id) {
+        Profile profile = profileRepository.findById(id)
+                .orElseThrow(() -> new ProfileNotFoundException(id));
 
+        if (!userService.hasAccessToProfile(profile)) {
+            throw new AccessDeniedException("You are not allowed to access this profile.");
+        }
+        return profileMapper.toResponse(profile);
+    }
 }
---
+++ src/main/java/com/sheoanna/teach_sphere/profile/exceptions/ProfileNotFoundException.java
@@ -0,0 +1,12 @@
+package com.sheoanna.teach_sphere.profile.exceptions;
+
+import com.sheoanna.teach_sphere.global.AppException;
+
+public class ProfileNotFoundException extends AppException {
+    public ProfileNotFoundException(Long id) {
+        super("Profile with ID: " + id +  "not found.");
+    }
+    public ProfileNotFoundException(String message) {
+        super(message);
+    }
+}

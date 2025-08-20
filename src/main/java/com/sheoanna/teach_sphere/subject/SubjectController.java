a0dfe2e9d94f71c0195724102d32ff6bd6bd556d
NepyAnna <nepyann@gmail.com>
2025-08-20 15:53:09 +0200

    feat: added handleSubjectNotFoundByIDException to GlobalExceptionHandler and findSubjectById to SubjectController

--- src/main/java/com/sheoanna/teach_sphere/subject/SubjectController.java
+++ src/main/java/com/sheoanna/teach_sphere/subject/SubjectController.java
@@ -1,10 +1,12 @@
 package com.sheoanna.teach_sphere.subject;
 
 import com.sheoanna.teach_sphere.subject.dtos.SubjectResponse;
+import com.sheoanna.teach_sphere.subject.dtos.SubjectResponseWithMentorSub;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +21,8 @@ public class SubjectController {
                                                            @RequestParam(defaultValue = "4") int size){
         Pageable pageable = PageRequest.of(page, size);
         return ResponseEntity.ok().body(subjectService.findAllSubjects(pageable));
+    }
+    public ResponseEntity<SubjectResponseWithMentorSub> findSubjectById(@PathVariable Long id) {
+        return ResponseEntity.ok().body(subjectService.findSubjectById(id));
     }
 }

5ce53bb1fafc02488ea99b57724e3b2c3d8b8ccc
NepyAnna <nepyann@gmail.com>
2025-09-01 17:10:37 +0200

    feat: added MentorSubjectReview class and dtos with mapper

---
+++ src/main/java/com/sheoanna/teach_sphere/review/dtos/MentorSubjectReviewMapper.java
@@ -0,0 +1,12 @@
+package com.sheoanna.teach_sphere.review.dtos;
+
+import com.sheoanna.teach_sphere.mentor_subject.dtos.MentorSubjectRequest;
+import com.sheoanna.teach_sphere.review.MentorSubjectReview;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MentorSubjectReviewMapper {
+    public MentorSubjectReview toEntity(MentorSubjectRequest request) {
+        return MentorSubjectReview.
+    }
+}
